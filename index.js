fbe50cd2bea74ea11898b91661222b34f0cf1037
Guilherme <guilhermepolicarpio@gmail.com>
2022-08-26 13:32:34 -0300

    Implementação do envio de usuarios e tweets ao serservidor

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ import express from 'express';
 const app = express(); 
 app.use(express.json());
 
+const tweets=[];
+const users=[];
 
-app.get("/", (req, res) => {
 
-    res.send('Hello World');
+app.post('/sign-up', (req,res) => {
+    const user = req.body;
+    users.push(user);
+    res.send("OK")
+
 });
 
+app.post('/tweets', (req,res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send("OK")
+
+});
+
+app.get('/sign-up', (req, res) => {
+    res.send(users);
+  });
+
+  app.get('/tweets', (req, res) => {
+    res.send(tweets);
+  });
 
 app.listen(5000 ,() => console.log("ola"));
