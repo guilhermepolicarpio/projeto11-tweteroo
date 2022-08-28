7d08fd8e5d8c31886371076713620fe9d20baec4
Guilherme <guilhermepolicarpio@gmail.com>
2022-08-28 10:52:21 -0300

    Implementação da visualização dos últimos 10 tweets

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 import express from 'express';
+import cors from "cors";
 
 const app = express(); 
 app.use(express.json());
+app.use(cors());
 
 const tweets=[];
 const users=[];
 
 
 app.post('/sign-up', (req,res) => {
-    const user = req.body;
-    users.push(user);
-    res.send("OK")
+    const {username,avatar} = req.body;
 
+    users.push(({
+      id: users.length,
+      username,
+      avatar
+    }));
+    res.send("OK")
 });
 
 app.post('/tweets', (req,res) => {
-    const tweet = req.body;
+
+    const tweet = {
+      username: req.body.username,
+      tweet: req.body.tweet,
+      avatar: users.find((user) => user.username === req.body.username).avatar,
+    };
     tweets.push(tweet);
     res.send("OK")
-
 });
 
 app.get('/sign-up', (req, res) => {
     res.send(users);
   });
 
-  app.get('/tweets', (req, res) => {
-    res.send(tweets);
-  });
+app.get('/tweets', (req, res) => {
+  var last10=[];
+  tweets.reverse();
+  if(tweets.length>10){
+    for(let i=0 ; i < 10 ; i++){
+      last10.push(tweets[i]);
+    }
+  }
+  else{
+    last10=tweets;
+  }
+  res.send(last10);
+  tweets.reverse();
+});
 
-app.listen(5000 ,() => console.log("ola"));
+app.listen(5000 ,() => console.log("Executando na porta 5000"));
